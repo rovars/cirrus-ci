4225742d1da7eb36d367d3725f256313d1009ef7
llcpp <llcpp@users.noreply.github.com>
2025-09-01 16:40:50 +0700

    (#105) 2025-09-01 16:40:50

--- build.sh
+++ build.sh
@@ -6,7 +6,27 @@ setup_src() {
     mkdir -p .repo/local_manifests/
     mv romx/patch/remove.xml .repo/local_manifests/
     repo sync -j16 -c --force-sync --no-clone-bundle --no-tags --prune
-    ./romx/patches.sh   
+
+    rm -rf frameworks/base/packages/OsuLogin \
+       frameworks/base/packages/PrintRecommendationService
+
+    declare -A PATCHES=(
+        ["art"]="android_art/0001-constify_JNINativeMethod.patch"
+        ["external/conscrypt"]="android_external_conscrypt/0001-constify_JNINativeMethod.patch"
+        ["frameworks/base"]="android_frameworks_base/0018-constify_JNINativeMethod.patch"
+        ["frameworks/opt/net/wifi"]="android_frameworks_opt_net_wifi/0001-constify_JNINativeMethod.patch"
+        ["libcore"]="android_libcore/0004-constify_JNINativeMethod.patch"
+        ["packages/apps/Nfc"]="android_packages_apps_Nfc/0001-constify_JNINativeMethod.patch"
+        ["packages/apps/Bluetooth"]="android_packages_apps_Bluetooth/0001-constify_JNINativeMethod.patch"
+        ["prebuilts/abi-dumps/vndk"]="android_prebuilts_abi-dumps_vndk/0001-protobuf-avi.patch"
+    )
+
+    for target_dir in "${!PATCHES[@]}"; do
+        patch_file="${PATCHES[$target_dir]}"
+        cd "$target_dir" || exit
+        git am "$WORKDIR/Axp/Patches/LineageOS-17.1/$patch_file"
+        cd "$WORKDIR"
+    done   
 }
 
 build_src() {
