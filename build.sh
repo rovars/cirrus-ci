e43541b82b0ea5b18e825921de309c987ce0b857
bimuafaq <bimuafaq@users.noreply.github.com>
2025-09-22 12:20:09 +0700

    (#83) 2025-09-22 12:20:09

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ setup_src() {
     repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
     git clone -q https://github.com/rovars/rom romx
     mkdir -p .repo/local_manifests
+    mv romx/A11/los/patch.sh .
     mv romx/A11/los/* .repo/local_manifests
     retry_rc repo sync -c -j16 --force-sync --no-clone-bundle --no-tags --prune
-    source romx/A11/los/patch.sh
+    source patch.sh
 }
 
 build_src() {    
