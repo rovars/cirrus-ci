84729abdde2d1697a803543c7ebcfd804f315ef2
bimuafaq <bimuafaq@users.noreply.github.com>
2025-09-23 09:57:23 +0700

    (#100) 2025-09-23 09:57:23

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ setup_src() {
     repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
     git clone -q https://github.com/rovars/rom romx
     mkdir -p .repo/local_manifests
-    mv romx/A11/los/patch.sh .
+    mv romx/A11/los/patch n_patch
     mv romx/A11/los/* .repo/local_manifests
     retry_rc repo sync -c -j16 --force-sync --no-clone-bundle --no-tags --prune
-    source romx/create-signed-env.sh
-    source patch.sh
-    
+    source n_patch/patch.sh
 }
 
 build_src() {
@@ -17,7 +15,7 @@ build_src() {
 
   export PRODUCT_DISABLE_SCUDO=true
   export SKIP_ABI_CHECKS=true
-  export OWN_KEYS_DIR=$WORKDIR/romx/keys
+  export OWN_KEYS_DIR=$WORKDIR/romx/A11/keys
   export RELEASE_TYPE=UNOFFICIAL-GmsCompat-signed
 
   if [ ! -e $OWN_KEYS_DIR/testkey.pk8 ] ; then
