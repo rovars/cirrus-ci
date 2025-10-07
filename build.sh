0616fd91a0ebb341a924586016ecb552e83f2b7c
bimuafaq <bimuafaq@users.noreply.github.com>
2025-10-07 08:32:46 +0700

    (#128) 2025-10-07 08:32:46

--- build.sh
+++ build.sh
@@ -2,28 +2,27 @@
 
 setup_src() {
     repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
-
     git clone -q https://github.com/rovars/rom romx
 
     mkdir -p .repo/local_manifests
-    mv romx/manifest/lin11* .repo/local_manifests/
+    mv romx/script/rom/lin11* .repo/local_manifests/
 
     retry_rc repo sync -c -j16 --force-sync --no-clone-bundle --no-tags --prune
 
     zpatch=$SRC_DIR/z_patches
-    xpatch=$SRC_DIR/romx/patch
+    xpatch=$SRC_DIR/romx/script/rom/patch
 
     patch -p1 < $xpatch/lin11-allow-permissive-user-build.patch
 
     cd vendor/lineage
     git am $zpatch/patch_002_vendor-lineage.patch
     git am $zpatch/patch_004_vendor-lineage.patch
-    git am $xpatch/lin11-Vendor*.patch
+    git am $xpatch/lin11-vendor*.patch
     cd $SRC_DIR
 
     cd frameworks/base
     git am $zpatch/patch_001_base.patch
-    git am $xpatch/lin11-Base*.patch
+    git am $xpatch/lin11-base*.patch
     cd $SRC_DIR
 
     cd packages/apps/Settings
@@ -34,6 +33,7 @@ setup_src() {
 
 build_src() {
     source build/envsetup.sh
+    set_remote_vars
 
     export SKIP_ABI_CHECKS=true
     export OWN_KEYS_DIR=$SRC_DIR/romx/keys
@@ -41,13 +41,25 @@ build_src() {
 
     [ ! -e $OWN_KEYS_DIR/testkey.pk8 ] && ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
     [ ! -e $OWN_KEYS_DIR/testkey.x509.pem ] && ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
-
-    set_remote_vars
+    
     brunch RMX2185 user
 }
 
 upload_src() {
-    upSrc="out/target/product/*/*-RMX*.zip"
-    mkdir -p ~/.config && mv romx/config/* ~/.config || true
-    timeout 15m telegram-upload $upSrc --caption "${CIRRUS_COMMIT_MESSAGE}" --to $idtl || true
+    REPO="rovars/vars"
+    RELEASE_TAG="lineage-18.1"
+    ROM_FILE=$(find out/target/product -name "*-RMX*.zip" -print -quit)
+    ROM_X="https://github.com/$REPO/releases/download/$RELEASE_TAG/$(basename "$ROM_FILE")"
+
+    echo "$tokenpat" > tokenpat.txt
+    gh auth login --with-token < tokenpat.txt
+
+    if ! gh release view "$RELEASE_TAG" -R "$REPO" > /dev/null 2>&1; then
+        gh release create "$RELEASE_TAG" -t "$RELEASE_TAG" -R "$REPO" --generate-notes
+    fi
+
+    gh release upload "$RELEASE_TAG" "$ROM_FILE" -R "$REPO" --clobber
+
+    echo "$ROM_X"
+    xc -s "$MSG_XC2"
 }
--- main.sh
+++ main.sh
@@ -4,6 +4,10 @@ set -e
 export USE_DEX2OAT_DEBUG=false
 export WITH_DEXPREOPT_DEBUG_INFO=false
 export NINJA_HIGHMEM_NUM_JOBS=1
+export DISABLE_ROBO_RUN_TESTS=true
+
+MSG_XC1="( <a href='https://cirrus-ci.com/task/${CIRRUS_TASK_ID}'>Cirrus CI</a> ) - $CIRRUS_COMMIT_MESSAGE ( $CIRRUS_BRANCH )"
+MSG_XC2="( <a href='https://cirrus-ci.com/task/${CIRRUS_TASK_ID}'>Cirrus CI</a> ) - $CIRRUS_COMMIT_MESSAGE ( <a href='$ROM_X'>$(basename "$CIRRUS_BRANCH")</a> )"
 
 source "$PWD/build.sh"
 
@@ -11,14 +15,15 @@ set_ccache_vars() {
     export USE_CCACHE=1
     export CCACHE_EXEC="$(command -v ccache)"
     export CCACHE_DIR="$HOME/.ccache"
-    ccache -M 50G -F 0
+    ccache -M 50G -F 0 &> /dev/null
+    ccache -o compression=true &> /dev/null
 }
 
 retry_rc() {
-    local max_retries=20
+    local max_retries=12
     local delay=5
     local attempt=1
-    
+
     while [[ $attempt -le $max_retries ]]; do
         "$@" && return 0
         [[ $attempt -lt $max_retries ]] && sleep "$delay"
@@ -27,102 +32,79 @@ retry_rc() {
     return 1
 }
 
-copy_cache() {
+setup_cache() {
     mkdir -p ~/.ccache
     cd ~/
     set_ccache_vars
-    if retry_rc rclone copy "$rclonedir/$rclonefile" . --progress; then
+    if retry_rc rclone copy "$rclonedir/$rclonefile" . &> /dev/null; then
         tar -xzf "$rclonefile" -C .
         rm -f "$rclonefile"
+        echo "===== ccache setup done ====="
+        xc -s "$MSG_XC1
+( ccache setup done )"
     else
         rm -f "$rclonefile"
-        xc -x "no remote ccache!"
+        echo "===== no ccache? ah skip ====="
+        xc -s "$MSG_XC1
+( no ccache? ah skip )"
     fi
+    cd $SRC_DIR
 }
 
 save_cache() {
     export CCACHE_DISABLE=1
     ccache -s    
-    ccache --cleanup
-    ccache --zero-stats
+    ccache --cleanup &> /dev/null
+    ccache --zero-stats &> /dev/null
 
     cd ~/
     tar -czf "$rclonefile" -C . .ccache --warning=no-file-changed || {
-        xc -x "Failed to create cache archive"
+        xc -x "create ccache archive failure!"
         return 1
     }
 
-    if retry_rc rclone copy "$rclonefile" "$rclonedir" --progress; then
+    if retry_rc rclone copy "$rclonefile" "$rclonedir" &> /dev/null; then
         rm -f "$rclonefile"
+        echo "===== ccache save success ====="
+        xc -s "$MSG_XC2
+( ccache save success )"
     else
-        xc -x "Failed to copy cache to remote"
+        echo "===== ccache save failure ====="
+        xc -s "$MSG_XC2
+( ccache save failure )"
         return 1
     fi
+    cd $SRC_DIR
 }
 
 set_remote_vars() {
-git clone -q https://github.com/rovars/reclient
-mkdir -p /tmp/rbe_log_dir
+    echo "===== Remote Build Execution ====="
+    git clone -q https://github.com/rovars/reclient
 
-unset USE_CCACHE CCACHE_EXEC CCACHE_DIR USE_GOMA
-
-export USE_RBE=1
-export RBE_DIR="reclient"
-export RBE_instance="rovars.buildbuddy.io"
-export RBE_service="rovars.buildbuddy.io:443"
-export RBE_remote_headers="x-buildbuddy-api-key=yaDX7CznLv0XcEqk0wee"
-
-export RBE_R8_EXEC_STRATEGY=remote_local_fallback
-export RBE_CXX_EXEC_STRATEGY=remote_local_fallback
-export RBE_D8_EXEC_STRATEGY=remote_local_fallback
-export RBE_JAVAC_EXEC_STRATEGY=remote_local_fallback
-export RBE_JAR_EXEC_STRATEGY=remote_local_fallback
-export RBE_ZIP_EXEC_STRATEGY=remote_local_fallback
-export RBE_TURBINE_EXEC_STRATEGY=remote_local_fallback
-export RBE_SIGNAPK_EXEC_STRATEGY=remote_local_fallback
-export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback
-export RBE_ABI_LINKER_EXEC_STRATEGY=remote_local_fallback
-export RBE_CLANG_TIDY_EXEC_STRATEGY=remote_local_fallback
-export RBE_METALAVA_EXEC_STRATEGY=remote_local_fallback
-export RBE_LINT_EXEC_STRATEGY=remote_local_fallback
-export RBE_ABI_DUMPER_EXEC_STRATEGY=""
-
-export RBE_JAVAC=1
-export RBE_R8=1
-export RBE_D8=1
-export RBE_JAR=1
-export RBE_ZIP=1
-export RBE_TURBINE=1
-export RBE_SIGNAPK=1
-export RBE_CXX_LINKS=1
-export RBE_CXX=1
-export RBE_ABI_LINKER=1
-export RBE_CLANG_TIDY=1
-export RBE_METALAVA=1
-export RBE_LINT=1
-export RBE_ABI_DUMPER=""
-
-export RBE_JAVA_POOL=default
-export RBE_METALAVA_POOL=default
-export RBE_LINT_POOL=default
-export RBE_log_dir="/tmp/rbe_log_dir"
-export RBE_output_dir="/tmp/rbe_log_dir"
-export RBE_proxy_log_dir="/tmp/rbe_log_dir"
-export RBE_service_no_auth=true
-export RBE_use_rpc_credentials=false
-export RBE_use_unified_cas_ops=true
-export RBE_use_unified_downloads=true
-export RBE_use_unified_uploads=true
-export RBE_use_application_default_credentials=true
+    unset USE_CCACHE CCACHE_EXEC CCACHE_DIR USE_GOMA
+    export USE_RBE=1 RBE_DIR="reclient" RBE_instance="rovars.buildbuddy.io" RBE_service="rovars.buildbuddy.io:443" RBE_remote_headers="x-buildbuddy-api-key=yaDX7CznLv0XcEqk0wee"
+    export RBE_R8_EXEC_STRATEGY=remote_local_fallback RBE_CXX_EXEC_STRATEGY=remote_local_fallback RBE_D8_EXEC_STRATEGY=remote_local_fallback RBE_JAVAC_EXEC_STRATEGY=remote_local_fallback
+    export RBE_JAR_EXEC_STRATEGY=remote_local_fallback RBE_ZIP_EXEC_STRATEGY=remote_local_fallback RBE_TURBINE_EXEC_STRATEGY=remote_local_fallback RBE_SIGNAPK_EXEC_STRATEGY=remote_local_fallback
+    export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback RBE_ABI_LINKER_EXEC_STRATEGY=remote_local_fallback RBE_CLANG_TIDY_EXEC_STRATEGY=remote_local_fallback RBE_METALAVA_EXEC_STRATEGY=remote_local_fallback
+    export RBE_LINT_EXEC_STRATEGY=remote_local_fallback RBE_ABI_DUMPER_EXEC_STRATEGY=""
+    export RBE_JAVAC=1 RBE_R8=1 RBE_D8=1 RBE_JAR=1 RBE_ZIP=1 RBE_TURBINE=1 RBE_SIGNAPK=1 RBE_CXX_LINKS=1 RBE_CXX=1
+    export RBE_ABI_LINKER=1 RBE_CLANG_TIDY=1 RBE_METALAVA=1 RBE_LINT=1 RBE_ABI_DUMPER=""
+    export RBE_JAVA_POOL=default RBE_METALAVA_POOL=default RBE_LINT_POOL=default
+    export RBE_log_dir="/tmp" RBE_output_dir="/tmp" RBE_proxy_log_dir="/tmp"
+    export RBE_service_no_auth=true RBE_use_rpc_credentials=false RBE_use_unified_cas_ops=true RBE_use_unified_downloads=true
+    export RBE_use_unified_uploads=true RBE_use_application_default_credentials=true
 }
 
 main() {
+    export SRC_DIR=$PWD/src
+    mkdir -p $SRC_DIR
     cd "$SRC_DIR"
     case "${1:-}" in
-        sync) setup_src ;;
+        sync) xc -s "$MSG_XC1"
+              setup_src ;;
         build) build_src ;;
         upload) upload_src ;;
-        copy_cache) copy_cache ;;
+        setup_cache) setup_cache ;;
         save_cache) save_cache ;;
         *)
             echo "Error: Invalid argument." >&2
