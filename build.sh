00d2622ae7d738c5f1b40e363219079a0080d0cc
rovars <rducks@duck.com>
2025-10-10 09:20:16 +0700

    (#160) 2025-10-10 09:20:16

--- build.sh
+++ build.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/env bash
 
 setup_src() {
-    repo init -u https://gitlab.e.foundation/e/os/android.git -b v1-s --git-lfs --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
+    repo init -u https://github.com/AICP/platform_manifest.git -b s12.1 --git-lfs --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
     git clone -q https://github.com/rovars/rom romx
     mkdir -p .repo/local_manifests
     mv romx/script/rom/12* .repo/local_manifests/
     retry_rc repo sync -c -j8 --force-sync --no-clone-bundle --no-tags --prune
 
     rm -rf external/chromium-webview
-    git clone -q --depth=1 https://github.com/LineageOS/android_external_chromium-webview -b master external/chromium-webview
-
-    cd prebuilts/prebuiltapks
-    git lfs pull
-    rm -rf Browser Notes Mail
-    cd $SRC_DIR
+    git clone -q --depth=1 https://github.com/LineageOS/android_external_chromium-webview -b master external/chromium-webview  
 
     xpatch=$SRC_DIR/romx/script/rom/patch
     zpatch=$SRC_DIR/romx/script/rom/patch/lin12
 
     patch -p1 < $xpatch/init_fatal_reboot_target_recovery.patch
-
-    cd frameworks/base
-    git am $xpatch/lin11-base-Revert-New-activity-transitions.patch
-    rm -rf $zpatch/patches_platform/frameworks_base/0011*
-    git am $zpatch/patches_platform/frameworks_base/0*
-    cd $SRC_DIR
-
+    patch -p1 < $xpatch/12*
+  
     cd system/core
     git am $zpatch/patches_treble_phh/platform_system_core/0001*
     git am $zpatch/patches_treble_phh/platform_system_core/0002*
     git am $zpatch/patches_treble_phh/platform_system_core/0003*
-    git am $zpatch/patches_treble_phh/platform_system_core/0006*    
+    git am $zpatch/patches_treble_phh/platform_system_core/0006*
     cd $SRC_DIR
 
     cd external/selinux
     git am $zpatch/patches_treble_phh/platform_external_selinux/0002-*
-    cd $SCR_DIR
+    cd $SRC_DIR
+
+    cd device/realme/RMX2185
+    sed -i 's/lineage_/aicp_/g' AndroidProducts.mk
+    sed -i 's/lineage_/aicp_/g' lineage_RMX2185.mk
+    sed -i 's|$(call inherit-product, vendor/lineage/config/common_full_phone.mk)|$(call inherit-product, vendor/aicp/config/common_full_phone.mk)|g' lineage_RMX2185.mk
+    mv lineage_RMX2185.mk aicp_RMX2185.mk
+    cd $SRC_DIR
+
 }
 
 build_src() {
@@ -47,8 +45,6 @@ build_src() {
     export RBE_R8_EXEC_STRATEGY="racing"
     export RBE_D8_EXEC_STRATEGY="racing"
 
-    export RELEASE_TYPE=community
-
     export SKIP_ABI_CHECKS=true    
     export OWN_KEYS_DIR=$SRC_DIR/romx/keys
 
@@ -56,15 +52,15 @@ build_src() {
     ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
 
     ln -sf "$OWN_KEYS_DIR" user-keys
-    sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\n\n;" "vendor/lineage/config/common.mk"
+    sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := user-keys/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := user-keys/releasekey\n\n;" "vendor/aicp/config/common.mk"
     
-    brunch RMX2185
+    brunch RMX2185 user
 }
 
 upload_src() {
     REPO="rovars/vars"
-    RELEASE_TAG="e/OS"
-    ROM_FILE=$(find out/target/product -name "*-RMX*.zip" -print -quit)
+    RELEASE_TAG="AICP"
+    ROM_FILE=$(find out/target/product -name "*UNOFFICIAL*.zip" -print -quit)
     ROM_X="https://github.com/$REPO/releases/download/$RELEASE_TAG/$(basename "$ROM_FILE")"
 
     echo "$tokenpat" > tokenpat.txt
