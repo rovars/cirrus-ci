de0ebf8cae3d6367c6c5140e3e3694538426b16e
rovars <rducks@duck.com>
2025-11-08 19:49:15 +0700

    (#228) 2025-11-08 19:49:15

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 setup_src() {
     repo init -u https://github.com/rovars/android.git -b exthm-11 --groups=all,-notdefault,-darwin,-mips --git-lfs --depth=1
 
-    git clone -q https://github.com/rovars/rom x
+    git clone -q https://github.com/rovars/rom xx
     mkdir -p  .repo/local_manifests
     mv x/11/ext.xml .repo/local_manifests/
 
@@ -12,7 +12,7 @@ setup_src() {
     rm -rf external/chromium-webview
     git clone -q https://github.com/LineageOS/android_external_chromium-webview external/chromium-webview -b master --depth=1
 
-    patch -p1 < x/11/allow-permissive-user-build.patch
+    patch -p1 < xx/11/allow-permissive-user-build.patch
 }
 
 build_src() {
@@ -28,12 +28,12 @@ build_src() {
     export RBE_service="nano.buildbuddy.io:443"
     export RBE_remote_headers="x-buildbuddy-api-key=$nanokeyvars"
 
-    export OWN_KEYS_DIR=x/keys
+    export OWN_KEYS_DIR=$PWD/xx/keys
 
     sudo ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
     sudo ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
 
-    brunch RMX2185 user 2>&1 | tee build.txt
+    brunch RMX2185 user
 }
 
 upload_src() {
@@ -55,7 +55,7 @@ upload_src() {
     MSG_XC2="( <a href='https://cirrus-ci.com/task/${CIRRUS_TASK_ID}'>Cirrus CI</a> ) - $CIRRUS_COMMIT_MESSAGE ( <a href='$ROM_X'>$(basename "$CIRRUS_BRANCH")</a> )"
     xc -s "$MSG_XC2"
 
-    mv x/config/* ~/.config
+    mv xx/config/* ~/.config
     timeout 15m telegram-upload $ROM_FILE --to $idtl --caption "$CIRRUS_COMMIT_MESSAGE"
     xc -c "build.txt"
 }
